8278fe3b652e44b2112af23a66ced0436c90955f
fadhellnandaputra <fadhellnandaputra@gmail.com>
2024-01-22 22:35:03 +0700

    first commit

---
+++ assets/payeasy_db.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS 'payeasy';
+CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TYPE role_type AS ENUM ('customer', 'merchant');
+CREATE TYPE status_type AS ENUM ('credit', 'debit');
+
+CREATE TABLE users(
+	id uuid_generate_v4(),
+	name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	number BIGINT NOT NULL,
+	address VARCHAR(50) NOT NULL,
+	role role_type NOT NULL,
+	balance BIGINT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE merchants(
+	id uuid_generate_v4() NOT NULL,
+	name_merchants VARCHAR(50) NOT NULL,
+	id_users uuid NOT NULL,
+	balance BIGINT,
+	FOREIGN KEY (id_users) REFERENCES users(id),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE trx_history(
+	id uuid_generate_v4() NOT NULL,
+	id_users uuid NOT NULL,
+	id_merchants uuid NOT NULL,
+	status_payment status_type,
+	total_amount BIGINT,
+	FOREIGN KEY (id_users) REFERENCES users(id),
+	FOREIGN KEY (id_merchants) REFERENCES merchants(id),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
---
+++ config/app_config.go
@@ -0,0 +1,14 @@
+package config
+
+const(
+	ApiGroup = "/api/v1"
+
+	// user
+	
+	// merchant
+
+	// auth
+	
+	// transaction
+
+)
